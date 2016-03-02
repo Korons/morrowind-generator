3a992e56c82ca51f72a46ad6b43e88ea82863f65
Korons <william.sweeting@vfemail.net>
2016-03-02 11:27:36 -0500

    Made code more readable, fixed bugs.
    I added comments to and broke up the code so it more readable. I also changed how skills and attributes are printed so that they will no longer be doubled (For example both your attributes being luck)

--- morrowind.py
+++ morrowind.py
@@ -2,28 +2,47 @@
 #aubergine
 from random import choice
 from random import randint
+from random import shuffle
 attributenum = randint(0,7)
-racenum = randint(0,9)
 skillnum = randint(0,26)
-signnum = randint(0,12)
-signs = ["tower", "shadow", "lover", "ritual", "atronach", "apprentice", "lord", "steed", "lady", "serpent", "thief", "mage", "warrior"]
-skills = ["heavy armour", "medium armour", "spear", "acrobatics", "armourer", "axe", "blunt", "long blade", "block", "light armour", "marksman", "sneak", "athletics", "hand-to-hand", "short blade", "unarmoured", "illusion", "mercantile", "speechcraft", "alchemy", "conjuration", "enchant", "security", "alteration", "destruction", "mysticism", "restoration"]
+
+# Birth signs
+signs = ["tower", "shadow", "lover", "ritual", "atronach", "apprentice", "lord", "steed", "lady", "serpent", "thief", "mage",
+ "warrior"]
+
+ # Skills
+skills = ["heavy armour", "medium armour", "spear", "acrobatics", "armourer", "axe", "blunt", "long blade",
+"block", "light armour", "marksman", "sneak", "athletics", "hand-to-hand", "short blade", "unarmoured", "illusion",
+"mercantile", "speechcraft", "alchemy", "conjuration", "enchant", "security", "alteration", "destruction", "mysticism", "restoration"]
+
+# Races
 races = ["altmer", "argonian", "bosmer", "breton", "dunmer", "imperial", "khajiit", "nord", "orc", "redguard"]
+
+# Attributes
 attributes = ["agility", "endurance", "intelligence", "luck", "personality", "speed", "strength", "willpower"]
-print("your race is " + races[racenum])
-print("your birthsign is " + signs[signnum])
+print("Your race is " + choice(races))
+print("\nYour birthsign is " + choice(signs))
+
+# These are for the skill loops below
 skillcalc = 0
 attributecalc = 0
-print("your major skills are as follows:")
+
+# These randomize the skills and attributes lists
+skill_order = skills[:]
+shuffle(skill_order)
+attribute_order = attributes[:]
+
+# We use .pop() so we don't print the same item twice
+print("\nYour major skills are as follows:")
 for skillcalc in range(0,5):
-	print(choice(skills))
+	print(skill_order.pop())
 	skillcalc + 1
-print("your minor skills are as follows:")
+print("\nYour minor skills are as follows:")
 skillcalc = 0
 for skillcalc in range(0,5):
-	print(choice(skills))
+	print(skill_order.pop())
 	skillcalc + 1
-print("your primary attributes are as follows:")
+print("\nYour primary attributes are as follows:")
 for attributecalc in range(0,2):
-	print(choice(attributes))
+	print(attribute_order.pop())
 	attributecalc + 1
